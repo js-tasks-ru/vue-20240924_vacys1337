0e87b621d084f1da2ea8dd64cd04817d7a24f59c
glo1337 <vacys1337@gmail.com>
2024-09-26 17:46:47 +0500

    Реализован компонент для отображения текущей даты в формате dateStyle: long

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,23 @@
 import { defineComponent, createApp } from 'vue'
+
+const simpleComponent = defineComponent({
+    name: 'DateComponent',
+    setup() {
+        const dateString = new Date().toLocaleDateString('en-EN', { 
+            dateStyle: 'long' 
+        });
+
+        return {
+            dateString,
+        }
+    },
+
+    template: `
+        <div>Сегодня {{ dateString }}</div>
+    `
+})
+
+const app = createApp(simpleComponent);
+app.mount('#app');
+
+export default app;
