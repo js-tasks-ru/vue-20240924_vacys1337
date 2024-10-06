16e4d8f7970ca9e6aae4ce86655114dd64cf14e3
glo1337 <vacys1337@gmail.com>
2024-10-06 12:40:14 +0500

    Реализовано приложение счётчик

--- 02-basics-2/10-counter/CounterApp.js
+++ 02-basics-2/10-counter/CounterApp.js
@@ -1,9 +1,34 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'CounterApp',
+  setup() {
+    const counterConfig = {
+      minValue: 0,
+      maxValue: 5,
+    };
 
-  setup() {},
+    const counterValue = ref(0);
+
+    const incrementCounterValue = () => {
+      if (counterValue.value < counterConfig.maxValue) {
+        counterValue.value++;
+      }
+    }
+
+    const decrementCounterValue = () => {
+      if (counterValue.value > counterConfig.minValue) {
+        counterValue.value--;
+      }
+    }
+
+    return {
+      counterConfig,
+      counterValue,
+      incrementCounterValue,
+      decrementCounterValue,
+    }
+  },
 
   template: `
     <div class="counter">
@@ -11,15 +36,18 @@ export default defineComponent({
         class="button button--secondary"
         type="button"
         aria-label="Decrement"
-        disabled
+        @click="decrementCounterValue"
+        :disabled="counterValue <= counterConfig.minValue"
       >➖</button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count">{{ counterValue }}</span>
 
       <button
         class="button button--secondary"
         type="button"
         aria-label="Increment"
+        @click="incrementCounterValue"
+        :disabled="counterValue >= counterConfig.maxValue"
       >➕</button>
     </div>
   `,
