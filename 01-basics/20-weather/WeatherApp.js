e2f916f4742756522d33cff87d51ae7663ddfa0d
glo1337 <vacys1337@gmail.com>
2024-10-06 12:19:10 +0500

    Закончена реализация компонента для вывода погоды в Средиземье

--- 01-basics/20-weather/WeatherApp.js
+++ 01-basics/20-weather/WeatherApp.js
@@ -3,45 +3,91 @@ import { getWeatherData, WeatherConditionIcons } from './weather.service.ts'
 
 export default defineComponent({
   name: 'WeatherApp',
+  setup() {
+    const ablsoluteZeroTemperature = 273.15;
+
+    const alertMessage = (alert) => {
+      return `${alert.sender_name}: ${alert.description}`;
+    };
+
+    const convertKelvinToCelsius = (tempInKelvin) => {
+      return (tempInKelvin - ablsoluteZeroTemperature).toFixed(1);
+    };
+
+    const pressureFormatted = (pressureRawValue) => {
+      return (pressureRawValue * 0.75).toFixed();
+    };
+
+    const parseTime = (timeString) => {
+      const [hours, minutes] = timeString.split(':').map(Number);
+      const date = new Date();
+      date.setHours(hours, minutes, 0, 0);
+      return date;
+    };
+
+    const isNight = (cityWeatherData) => {
+      const currentTime = parseTime(cityWeatherData.current.dt);
+      const sunriseTime = parseTime(cityWeatherData.current.sunrise);
+      const sunsetTime = parseTime(cityWeatherData.current.sunset);
+      return currentTime < sunriseTime || currentTime > sunsetTime;
+    };
+
+    const cityWeatherData = getWeatherData();
+
+    const weatherFormatted = cityWeatherData.map(cityWeather => ({
+      geographic_name: cityWeather.geographic_name,
+      current: {
+        ...cityWeather.current,
+        tempCelsius: `${convertKelvinToCelsius(cityWeather.current.temp)} °C`,
+        pressureFormatted: `${pressureFormatted(cityWeather.current.pressure)} мм рт. ст.`,
+        weatherIcon: WeatherConditionIcons[cityWeather.current.weather.id],
+        isNight: isNight(cityWeather)
+      },
+      alert: cityWeather.alert ? alertMessage(cityWeather.alert) : null,
+    }));
+
+    return {
+      weatherFormatted,
+    };
+  },
 
   template: `
     <div>
       <h1 class="title">Погода в Средиземье</h1>
-
       <ul class="weather-list unstyled-list">
-        <li class="weather-card weather-card--night">
-          <div class="weather-alert">
+        <li v-for="cityWeather in weatherFormatted" class="weather-card" :class="{'weather-card--night': cityWeather.current.isNight}">
+          <div v-if="cityWeather.alert" class="weather-alert">
             <span class="weather-alert__icon">⚠️</span>
-            <span class="weather-alert__description">Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма.</span>
+            <span class="weather-alert__description">{{ cityWeather.alert }}</span>
           </div>
           <div>
             <h2 class="weather-card__name">
-              Гондор
+              {{ cityWeather.geographic_name }}
             </h2>
             <div class="weather-card__time">
-              07:17
+              {{ cityWeather.current.dt }}
             </div>
           </div>
           <div class="weather-conditions">
-            <div class="weather-conditions__icon" title="thunderstorm with heavy rain">⛈️</div>
-            <div class="weather-conditions__temp">15.0 °C</div>
+            <div class="weather-conditions__icon" :title="cityWeather.current.weather.description">{{ cityWeather.current.weatherIcon }}</div>
+            <div class="weather-conditions__temp">{{ cityWeather.current.tempCelsius }}</div>
           </div>
           <div class="weather-details">
             <div class="weather-details__item">
               <div class="weather-details__item-label">Давление, мм рт. ст.</div>
-              <div class="weather-details__item-value">754</div>
+              <div class="weather-details__item-value">{{ cityWeather.current.pressureFormatted }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Влажность, %</div>
-              <div class="weather-details__item-value">90</div>
+              <div class="weather-details__item-value">{{ cityWeather.current.humidity }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Облачность, %</div>
-              <div class="weather-details__item-value">100</div>
+              <div class="weather-details__item-value">{{ cityWeather.current.clouds }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Ветер, м/с</div>
-              <div class="weather-details__item-value">10.5</div>
+              <div class="weather-details__item-value">{{ cityWeather.current.wind_speed }}</div>
             </div>
           </div>
         </li>
