5cd8ef5678e7c83d9f6a3840f5bcad7d490cf19f
glo1337 <vacys1337@gmail.com>
2024-10-08 18:39:10 +0500

    Реализован компонент MeetupView для отображения информации о митапе

--- 03-components/10-MeetupView/MeetupView.js
+++ 03-components/10-MeetupView/MeetupView.js
@@ -12,12 +12,26 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupCover,
+    MeetupAgenda,
+    MeetupDescription,
+    MeetupInfo,
   },
 
+  props: {
+    meetup: {
+      type: Object,
+    }
+  },
+  
   template: `
     <div>
 
       <!-- Обложка митапа -->
+      <MeetupCover 
+        :title="meetup.title"
+        :image="meetup.image"
+      />
 
       <UiContainer>
         <div class="meetup">
@@ -25,18 +39,30 @@ export default defineComponent({
             <h2>Описание</h2>
 
             <!-- Описание митапа -->
+            <MeetupDescription
+              :description="meetup.description"
+            />
 
             <h2>Программа</h2>
 
             <!-- Программа митапа -->
             <!-- Или при пустой программе - сообщение "Программа пока пуста..." в UiAlert -->
-            <UiAlert></UiAlert>
+            <template v-if="meetup.agenda.length">
+              <MeetupAgenda 
+                :agenda="meetup.agenda"
+              />
+            </template>
+            <UiAlert v-else>Программа пока пуста...</UiAlert>
 
           </div>
           <div class="meetup__aside">
 
             <!-- Краткая информация о митапе -->
-
+            <MeetupInfo 
+              :organizer="meetup.organizer"
+              :place="meetup.place"
+              :date="meetup.date"
+            />
             <div class="meetup__aside-buttons"></div>
           </div>
         </div>
