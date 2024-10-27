b7d3ec483770bb2a2be634ab1299edbdbabe72df
javascriptru <67475495+jsru-1@users.noreply.github.com>
2024-10-27 14:07:57 +0300

    Added teacher solution for task: MeetupView в `script setup`

---
+++ 04-sfc/10-MeetupView-script-setup/MeetupAgenda.vue
@@ -0,0 +1,34 @@
+<script setup>
+import MeetupAgendaItem from './MeetupAgendaItem.vue'
+
+defineProps({
+  agenda: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <ul class="agenda">
+    <li v-for="agendaItem in agenda" :key="agendaItem.id" class="agenda__item">
+      <MeetupAgendaItem :agenda-item="agendaItem" />
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.agenda {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.agenda__item {
+  border-top: 1px solid var(--grey-3);
+}
+
+.agenda__item:first-child {
+  border-top: none;
+}
+</style>
---
+++ 04-sfc/10-MeetupView-script-setup/MeetupDescription.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  description: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="meetup-description">{{ description }}</div>
+</template>
+
+<style scoped>
+.meetup-description {
+  padding-top: 33px;
+  margin-bottom: 24px;
+  font-size: 18px;
+  line-height: 28px;
+  white-space: pre-wrap;
+}
+</style>
